d24540eaca97d8a81fed89025d09d7da6f4f4e35
Alejandro Rangel <ale.ran92@gmail.com>
2025-07-09 11:34:30 -0500

    correo nuevo somoseureka para solicitud a tenciones

--- php/solicitud_recurso.php
+++ php/solicitud_recurso.php
@@ -124,17 +124,17 @@
 		//Server settings
 		//$mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL;                      // OFF verbose debug output
 		$mail->isSMTP();                                            // Send using SMTP
-	    $mail->Host       = 'mail.eurekalibros.com.co';                    // Set the SMTP server to send through
+	    $mail->Host       = 'mail.somoseureka.com.co';                    // Set the SMTP server to send through
 	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 	    $mail->SMTPAutoTLS = false; 
-	    $mail->Username   = 'crm@eurekalibros.com.co';                     // SMTP username
+	    $mail->Username   = 'crm@somoseureka.com.co';                     // SMTP username
 	    $mail->Password   = 'cRm14356$';                              // SMTP password
 		//$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_S	above                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 		//Recipients
-		$mail->setFrom('crm@eurekalibros.com.co', 'CRM Eureka');
-		$mail->addAddress("felipe.vargas@eurekalibros.com.co", 'felipe.vargas@eurekalibros.com.co');     // Add a recipient
+		$mail->setFrom('crm@somoseureka.com.co', 'CRM Eureka');
+		$mail->addAddress("felipe.vargas@somoseureka.com.co", 'felipe.vargas@somoseureka.com.co');     // Add a recipient
 			  
 		//$mail->addCC("oltoledo@hotmail.com");
 		//$mail->addBCC('comercial@eurekalibros.com.co');
