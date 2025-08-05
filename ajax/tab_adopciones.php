f5296f2e75d189eec9742f0782c63127690e6a41
Alejandro Rangel <ale.ran92@gmail.com>
2025-08-05 16:52:57 -0500

    desactivado errores

--- ajax/tab_adopciones.php
+++ ajax/tab_adopciones.php
@@ -1,9 +1,9 @@
 <?php
-	ini_set("display_errors", 1);
+	/*ini_set("display_errors", 1);
 
 	ini_set("display_startup_errors", 1);
 
-	error_reporting(E_ALL);
+	error_reporting(E_ALL);*/
 
 	require_once("../php/aut.php");
   	include("../conexion/bdd.php");
