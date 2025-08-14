9df15ee1654ee562a945b0dd0983179a051ba208
Alejandro Rangel <ale.ran92@gmail.com>
2025-08-14 10:56:23 -0500

    correo de pedidos ajustados

--- php/pedido_sa.php
+++ php/pedido_sa.php
@@ -148,27 +148,27 @@
 	$promo = $req_l2->fetch();
 
 	
-	/*$mail = new PHPMailer(true);
+	$mail = new PHPMailer(true);
 
 	try {
 
 		//Server settings
 		//$mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL;                      // OFF verbose debug output
 		$mail->isSMTP();                                            // Send using SMTP
-	    $mail->Host       = 'mail.eurekalibros.com.co';                    // Set the SMTP server to send through
+	    $mail->Host       = 'mail.somoseureka.com.co';                    // Set the SMTP server to send through
 	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 	    $mail->SMTPAutoTLS = false; 
-	    $mail->Username   = 'crm@eurekalibros.com.co';                     // SMTP username
+	    $mail->Username   = 'crm@somoseureka.com.co';                     // SMTP username
 	    $mail->Password   = 'cRm14356$';                              // SMTP password
 		//$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_S	above                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 		//Recipients
-		$mail->setFrom('crm@eurekalibros.com.co', 'CRM Eureka');
-		$mail->addAddress("felipe.vargas@eurekalibros.com.co", 'felipe.vargas@eurekalibros.com.co');     // Add a recipient
+		$mail->setFrom('crm@somoseureka.com.co', 'CRM Eureka');
+		$mail->addAddress("felipe.vargas@somoseureka.com.co", 'felipe.vargas@somoseureka.com.co');     // Add a recipient
 			  
-		$mail->addReplyTo('crm@eurekalibros.com.co', 'CRM Eureka');
-		$mail->addCC("comercial@eurekalibros.com.co");
+		$mail->addReplyTo('crm@somoseureka.com.co', 'CRM Eureka');
+		$mail->addCC("comercial@somoseureka.com.co");
 
 			  
 		// Content
@@ -180,11 +180,11 @@
 		$req->execute();
 		$pedido = $req->fetch();
 		                                  // Set email format to HTML
-		$mail->Subject = 'Solicitud de pedido distribuidor #'.$pedido["id"].'';
+		$mail->Subject = 'Solicitud de pedido sin adopcion #'.$pedido["id"].'';
 
 		
 
-		$mail->Body    = '<p style="font-size: 17px;">El usuario: '.$promo["promotor"].' hizo la solicitud de pedido #'.$pedido["id"].' para: '.$_POST["colegio"].'. Haz clic <a href="https://eurekalibros.com.co/promotores/pedido_colegio2.php?id_pedido_dist='.$pedido['id'].' ">aquí</a> para revisarlo<p>';
+		$mail->Body    = '<p style="font-size: 17px;">El usuario: '.$promo["promotor"].' hizo la solicitud de pedido #'.$pedido["id"].' para: '.$_POST["colegio"].'. Haz clic <a href="https://crm.somoseureka.com.co/pedido_colegio_sa.php?id_pedido_dist='.$pedido['id'].' ">aquí</a> para revisarlo<p>';
 
 		$mail->AltBody = 'probandosss';
 
@@ -195,7 +195,7 @@
 	} catch (Exception $e) {
 
 		echo "An error has occurred please try again: {$mail->ErrorInfo}";
-	}*/
+	}
 
 
 	header("Location: ../pedido_colegio_sa.php?id_pedido=".$pedido["id"]."");
