188ccee3aa841ff4b635ff6864429564f930211c
Alejandro Rangel <ale.ran92@gmail.com>
2025-09-03 10:23:42 -0500

    limite subida archivo a op

--- solicitar_op.php
+++ solicitar_op.php
@@ -639,6 +639,28 @@
 
       
   })
+
+    // Tamaño maximo del archivo
+      const maxSize = 10000000; 
+
+      // Obtener referencia al elemento
+      const $miInput = document.querySelector("#archivo");
+
+      $miInput.addEventListener("change", function () {
+          // si no hay archivos, regresamos
+          if (this.files.length <= 0) return;
+
+          // Validamos el primer archivo únicamente
+          const archivo = this.files[0];
+          if (archivo.size > maxSize) {
+              const tamanioEnMb = maxSize / 1000000;
+              alert(`El tamaño máximo es ${tamanioEnMb} MB`);
+              // Limpiar
+              $miInput.value = "";
+          } else {
+              // Validación pasada. Envía el formulario o haz lo que tengas que hacer
+          }
+      });
     </script>
     
   </body>
