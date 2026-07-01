b5183050e857f898df928af9438bb0bba34f2bb4
lauragodoy17 <godoylaura332@gmail.com>
2026-07-01 10:22:45 -0500

    Conexión a la base de datos de Panamá

--- conexion/bdd.php
+++ conexion/bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=crm_db;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=crm_panama;charset=utf8', 'root', '');
 	date_default_timezone_set('America/Bogota');
 }
 catch(Exception $e)
